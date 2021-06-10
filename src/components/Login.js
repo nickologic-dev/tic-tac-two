3c0d041da10396523cee5bdbfd397567e255b0f3
nbeilis <nicole.beilis@gmail.com>
2021-06-09 23:08:27 -0400

    add text fields

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import {TextField, Button} from '@material-ui/core';
 
 const Login = () => {
+  const [nickname, setNickname] = useState("");
+
+  const handleName = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const handleStart = () => {
+    if (nickname === "") {
+      console.log("boo", nickname);
+    } else {
+      console.log("yay", nickname);
+    }
+  }
+
   return (
-    <button className="square">
-      hello
-    </button>
+    <div>
+      <TextField id="outlined-basic" label="Enter nickname" variant="outlined" onChange={handleName} />
+      <Button onClick={handleStart} variant="contained">Create Game</Button>
+    {false && <TextField id="outlined-basic" label="Enter code" variant="outlined" onChange={handleName} />}
+    <Button onClick={handleStart} variant="contained">Join Game</Button>
+    </div>
   );
 }
 export default Login;
