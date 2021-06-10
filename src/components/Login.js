5bc5b839f297c2f86afc64f46c4218870dc101ba
nbeilis <nicole.beilis@gmail.com>
2021-06-09 22:08:00 -0400

    working state

---
+++ src/components/Login.js
@@ -0,0 +1,5 @@
+export default class Login {
+    render() {
+      return <h1>Hello</h1>;
+    }
+  }
---
+++ src/firebase.setup.js
@@ -0,0 +1,4 @@
+import { initializeApp } from 'firebase';
+import firebaseConfig from './firebase.config';
+const app = initializeApp(firebaseConfig);
+export default app;
